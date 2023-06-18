2c29a3131d10157caefbb7b710ca40a01a89b144
Suhail TH <suhailth17756@gmail.com>
2023-06-18 17:47:03 +0530

    first commit

---
+++ src/app/counter/state/counter.reducer.ts
@@ -0,0 +1,27 @@
+import { createReducer, on } from "@ngrx/store";
+import { initalState } from "./counter.state";
+import { decrement, increment, reset } from "./counter.action";
+
+
+const _counterReducer=createReducer(initalState,on(increment,(state)=>{
+    return {
+        ...state,
+        counter:state.counter+1
+    }
+}),
+on(decrement,(state)=>{
+    return {
+        ...state,
+        counter:state.counter-1
+    }
+}),
+on(reset,(state)=>{
+    return {
+        ...state,
+        counter:0
+    }
+}))
+
+export function counterReducer(state:any,action:any){
+    return  _counterReducer(state,action);
+}
---
+++ src/app/counter/state/counter.state.ts
@@ -0,0 +1,3 @@
+export const initalState={
+    counter:0
+}
